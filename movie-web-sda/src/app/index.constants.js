f7a4729d79b735dcb27283e77620e29b0e535504
Lucas Araújo <lucas.compufc@gmail.com>
2017-04-29 16:45:43 -0300

    Configurando projeto inicial.

---
+++ movie-web-sda/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('movieWebSda')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
