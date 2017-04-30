a6fc824229a139a6e37529e0ac3bad12c9a4fc72
Lucas Araújo <lucas.compufc@gmail.com>
2017-04-30 16:39:19 -0300

    Criando serviço para realizar login

---
+++ movie-web-sda/src/app/components/login/login.service.js
@@ -0,0 +1,51 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('movieWebSda')
+    .service('loginService', loginService);
+
+  /** @ngInject */
+  function loginService($window, $http, $httpParamSerializer, $cookies, OAuthProvider) {
+    var _data = {
+        grant_type: OAuthProvider.grantTypePassword, 
+        username: OAuthProvider.username, 
+        password: OAuthProvider.password
+    };
+
+    var _encoded = btoa(OAuthProvider.client + ":" + OAuthProvider.secret);
+
+    var _login =  function login() {   
+        var req = {
+            method: 'POST',
+            url: OAuthProvider.url,
+            headers: {
+                "Authorization": "Basic " + _encoded,
+                "Accept": "application/json",
+                "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+            },
+            data: $httpParamSerializer(_data)
+        }
+        
+        function loginSuccess(data) {
+             $http.defaults.headers.common.Authorization = 
+              'Bearer ' + data.data.access_token;
+            $cookies.put("access_token", data.data.access_token);
+            $window.location.href="#/home";
+        }
+
+        function loginFail(error) {
+            $log.error('Falha ao realizar o login');
+        }
+
+        return $http(req)
+            .then(loginSuccess)
+            .catch(loginFail);   
+    }
+
+    return {
+        login: _login
+    };
+
+  }
+})();
