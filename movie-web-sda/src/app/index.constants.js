f5417f61981f0efe16047550114a6f1a5b7f32cd
Lucas Araújo <lucas.compufc@gmail.com>
2017-04-30 16:38:15 -0300

    Criando constantes para OAuth

--- movie-web-sda/src/app/index.constants.js
+++ movie-web-sda/src/app/index.constants.js
@@ -5,6 +5,14 @@
   angular
     .module('movieWebSda')
     .constant('malarkey', malarkey)
-    .constant('moment', moment);
+    .constant('moment', moment)
+    .constant('OAuthProvider', {
+      url: 'http://localhost:8080/sda-services/oauth/token',
+      username: 'admin',
+      password: 'admin',
+      client: 'client',
+      secret: 'secret',
+      grantTypePassword: 'password'
+    });
 
 })();
