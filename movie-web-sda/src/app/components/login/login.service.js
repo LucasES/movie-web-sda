2d0d9fadb3bc3d908d516712deda5494d194bfe8
Lucas Araújo <lucas.compufc@gmail.com>
2017-05-01 17:19:48 -0300

    Alterando css do login

--- movie-web-sda/src/app/components/login/login.service.js
+++ movie-web-sda/src/app/components/login/login.service.js
@@ -6,10 +6,10 @@
     .service('loginService', loginService);
 
   /** @ngInject */
-  function loginService($window, $http, $httpParamSerializer, $cookies, OAuthProvider) {
+  function loginService($http, $httpParamSerializer, OAuthProvider) {
     var _encoded = btoa(OAuthProvider.client + ":" + OAuthProvider.secret);
 
-    var _login =  function login(login, password) {   
+    this.login =  function login(login, password) {           
         var _data = {
             grant_type: OAuthProvider.grantTypePassword, 
             username: login, 
@@ -25,26 +25,8 @@
             },
             data: $httpParamSerializer(_data)
         }
-        
-        function loginSuccess(data) {
-             $http.defaults.headers.common.Authorization = 
-              'Bearer ' + data.data.access_token;
-            $cookies.put("access_token", data.data.access_token);
-            $window.location.href="#/home";
-        }
 
-        function loginFail(error) {
-            $log.error('Falha ao realizar o login');
-        }
-
-        return $http(req)
-            .then(loginSuccess)
-            .catch(loginFail);   
+        return $http(req);   
     }
-
-    return {
-        login: _login
-    };
-
   }
 })();
