67b11cb53062d0a0047ffb3f5260bb8b25db5b1c
Lucas Araújo <lucas.compufc@gmail.com>
2017-05-01 00:19:49 -0300

    Criando layout do login e homepage

--- movie-web-sda/src/app/components/home/home.controller.js
+++ movie-web-sda/src/app/components/home/home.controller.js
@@ -7,9 +7,15 @@
 
   /** @ngInject */
   function HomeController(oauthFactory) {
+    var element = document.querySelector('body');
+    var angElement = angular.element(element);
+    angElement.addClass('body-class');
+    
     var _currentPage = "#/home";
 
     oauthFactory.checkLoggedUser(_currentPage);
+
+
     
   }
 })();
