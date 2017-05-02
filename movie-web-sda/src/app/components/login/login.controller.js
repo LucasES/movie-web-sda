4e411b9fdeb9745fd35fa4adb6270ed9f521d453
Lucas Araújo <lucas.compufc@gmail.com>
2017-05-01 21:18:22 -0300

    Alterando login do sistema para armazenar refresh token e adição do loading

--- movie-web-sda/src/app/components/login/login.controller.js
+++ movie-web-sda/src/app/components/login/login.controller.js
@@ -6,13 +6,25 @@
     .controller('LoginController', LoginController);
 
   /** @ngInject */
-  function LoginController(loginService) {
+  function LoginController($log, $window, $http, $cookies, loginService) {
     var vm = this;    
-    
+    vm.dataLoading = false;
     vm.login = login;
 
     function login () {
-      return loginService.login(vm.username, vm.password);
+      vm.dataLoading = true;
+      return loginService.login(vm.username, vm.password)
+        .then(function(data) {
+            vm.dataLoading = false;
+            $http.defaults.headers.common.Authorization = 
+            'Bearer ' + data.data.access_token;
+            $cookies.put("access_token", data.data.access_token);
+            $cookies.put("refresh_token", data.data.refresh_token);
+            $window.location.href="#/home";
+        }).catch(function() {
+           vm.dataLoading = false; 
+           $log.error('Falha ao realizar o login');
+        });
     }
   }
 })();
