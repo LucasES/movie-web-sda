24c544852bc61ee8315c6c5408e9f31550e63808
Lucas Araújo <lucas.compufc@gmail.com>
2017-05-01 22:14:16 -0300

    Adicionando endpoints

--- movie-web-sda/src/app/index.constants.js
+++ movie-web-sda/src/app/index.constants.js
@@ -8,11 +8,18 @@
     .constant('moment', moment)
     .constant('OAuthProvider', {
       url: 'http://localhost:8080/sda-services/oauth/token',
+      urlRefreshToken: 'http://localhost:8080/sda-services/oauth/token?grant_type=refresh_token&refresh_token=',
       username: 'admin',
-      password: 'admin',
+      password: 'password',
       client: 'client',
       secret: 'secret',
       grantTypePassword: 'password'
+    })
+    .constant('API', {
+      baseUrl: 'http://localhost:8080/sda-services/',
+      accessToken: '?access_token=',
+      getMovies: 'api/filme/',
+      getAllGenre: 'api/genero/'
     });
 
 })();
